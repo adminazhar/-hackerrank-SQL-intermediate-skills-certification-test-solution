923375617406ca0daef3abff4cd6960c91dd0203
Azhar Khan <admin@azhark.com>
2023-04-01 15:38:58 +0530

    Update product-sales-per-city.sql

--- product-sales-per-city.sql
+++ product-sales-per-city.sql
@@ -1,1 +1,5 @@
-
+SELECT ci.city_name, pr.product_name, ROUND(SUM(ii.line_total_price), 2) AS tot
+FROM city ci, customer cu, invoice i, invoice_item ii, product pr 
+WHERE ci.id = cu.city_id AND cu.id = i.customer_id AND i.id = ii.invoice_id AND ii.product_id = pr.id 
+GROUP BY ci.city_name, pr.product_name 
+ORDER BY tot DESC, ci.city_name, pr.product_name 
